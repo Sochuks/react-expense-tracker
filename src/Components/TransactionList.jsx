4e0912ae67b6dd16359519467b3f3a70c95cd900
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-04-26 17:52:44 +0100

    use context to get transaction objects

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalState'
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext)
+
   return (
+    
     <div>
-        <h3>History</h3>
-        <ul>
-           Cash <li><span>- $400</span> <button>Delete</button></li>
-        </ul>
+      {transactions.map((e)=>(
+        <div key={e.id}>
+            <h3>History</h3>
+            <ul>
+              {e.text} <li><span>{e.amount}</span> <button>Delete</button></li>
+            </ul>
+        </div>
+      ))};
+        
     </div>
   )
 }
