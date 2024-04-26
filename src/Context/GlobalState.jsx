667e834c89bcc448d1bdb4827f0b4bb0cfcd6ac1
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-04-27 00:01:55 +0100

    create a dispatch for delete to reducer

--- src/Context/GlobalState.jsx
+++ src/Context/GlobalState.jsx
@@ -19,6 +19,14 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({children}) =>{
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    // Actions
+    deleteTransaction = (id) =>{
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
     return (
         <GlobalContext.Provider 
         value={{transactions: state.transactions,
