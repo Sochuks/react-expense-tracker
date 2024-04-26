716c4cfea0cd68299423f9b0ae2277d3a9d19d65
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-04-26 12:51:09 +0100

    created the tarnsaction list components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Header from './Components/Header'
+import IncomeExpenses from './Components/IncomeExpenses'
+import TransactionList from './Components/TransactionList'
 
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
     <Header/>
+    <IncomeExpenses />
+    <TransactionList/>
     </div>
   )
 }
---
+++ src/Components/TransactionList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransactionList = () => {
+  return (
+    <div>
+        <h3>History</h3>
+        <ul>
+           Cash <li><span>- $400</span> <button>Delete</button></li>
+        </ul>
+    </div>
+  )
+}
+
+export default TransactionList
