3cdeb347796481f68e544a731627904463c4f10b
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-04-26 12:44:52 +0100

    created balance & IncomeExpenses component

---
+++ src/Components/Balance.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div>
+        <h4>Your Balance</h4>
+        <h2>$0.00</h2>
+    </div>
+  )
+}
+
+export default Balance
---
+++ src/Components/IncomeExpenses.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const IncomeExpenses = () => {
+  return (
+    <div>
+        {/* Income */}
+        <div>
+           <h4>Income</h4>
+           <p>+ $0.00</p>
+        </div>
+
+        {/* Expenses */}
+        <div>
+           <h4>Expenses</h4>
+           <p>- $0.00</p>
+        </div>
+    </div>
+  )
+}
+
+export default IncomeExpenses
