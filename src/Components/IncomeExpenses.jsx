ee30e646814bc317912a553a355345206711dcd1
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-04-26 23:39:36 +0100

    Created income & expense functions - Modify component styles

--- src/Components/IncomeExpenses.jsx
+++ src/Components/IncomeExpenses.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalState'
 
 const IncomeExpenses = () => {
+  const {transactions} = useContext(GlobalContext);
+
+  const amount = transactions.map(item => (item.amount))
+
+  // Get Income
+  const income = amount
+    .filter(item => item > 0)
+    .reduce((accumulate, item) => (accumulate += item), 0).toFixed(2)
+
+  // Get Expense
+  const expense = amount
+    .filter(item => item < 0)
+    .reduce((accumulate, item) => (accumulate += item), 0).toFixed(2)
+
   return (
-    <div>
+    <div className='w-full flex justify-between'>
         {/* Income */}
-        <div>
-           <h4>Income</h4>
-           <p>+ $0.00</p>
+        <div className='px-3 py-2 bg-green-400 w-full text-center text-white rounded-l-md border-r-2 border-slate-100'>
+           <h4 className='font-bold'>Income</h4>
+           <p className='text-lg'>{income}</p>
         </div>
 
         {/* Expenses */}
-        <div>
-           <h4>Expenses</h4>
-           <p>- $0.00</p>
+        <div className='px-3 py-2 bg-red-400 w-full text-center text-white rounded-r-md'>
+           <h4 className='font-medium'>Expenses</h4>
+           <p className='text-lg'>{expense}</p>
         </div>
     </div>
   )
