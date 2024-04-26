054d83ec110395d78141019b0bb433ef86818605
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-04-26 14:09:16 +0100

    add state managers for input and temporary validation

--- src/Components/AddTransaction.jsx
+++ src/Components/AddTransaction.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTransaction = () => {
+    // state management
+    const [text, setText] = useState('');
+    const [number, setNumber] = useState('');
+    
   return (
     <div>
         <h3>Add new transaction</h3>
@@ -9,9 +13,12 @@ const AddTransaction = () => {
                 {/* Text */}
                 <label htmlFor='text' className="block text-sm font-medium leading-5  text-gray-700">Name</label>
                     <div className="mt-1 relative rounded-md shadow-sm">
-                        <input placeholder="Enter Text" type="text"  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        <input placeholder="Enter Text" type="text"  
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" 
+                        value={text}
+                        onChange={(e)=>setText(e.target.value)}/>
                         {/* validation warning */}
-                        <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                        <div className={`inset-y-0 right-0 pr-3 flex items-center pointer-events-none ${text == '' ? 'absolute' : 'hidden'}`}>
                             <svg className="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
                                 <path fillRule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
@@ -25,9 +32,12 @@ const AddTransaction = () => {
                     <label htmlFor="amount" className="block text-sm font-medium leading-5  text-gray-700">Amount 
                     <br/> (negative - expense, positive - income) </label>
                         <div className="mt-1 relative rounded-md shadow-sm">
-                            <input placeholder="Enter Amount" type="number" className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            <input placeholder="Enter Amount" type="number" 
+                            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" 
+                            value={number}
+                            onChange={(e)=>setText(e.target.value)}/>
                             {/* validation warning */}
-                            <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                            <div className={`inset-y-0 right-0 pr-3 flex items-center pointer-events-none ${number == '' ? 'absolute' : 'hidden'}`}>
                                 <svg className="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
                                     <path fillRule="evenodd"
                                         d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
