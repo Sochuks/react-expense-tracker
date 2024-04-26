618e5db82326c95a3f8eb6f6bebfc0e612060ce5
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-04-26 18:03:52 +0100

    created transaction component

---
+++ src/Components/Transaction.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Transaction = () => {
+  return (
+    <div>
+        <div>
+            <h3>History</h3>
+            <ul>
+              {e.text} <li><span>{e.amount}</span> <button>Delete</button></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Transaction
