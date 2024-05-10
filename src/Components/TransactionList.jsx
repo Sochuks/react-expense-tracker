e29968cf8d20a5d338e9436ddabccb12402f959d
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-05-10 18:07:40 +0100

    Modified transactionList added search input

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -6,7 +6,15 @@ const TransactionList = () => {
   const { transactions } = useContext(GlobalContext)
 
   return (
-    <div>
+    <div className='flex flex-col max-w-md'>
+        <div className='py-1 font-semibold text-center'>
+          <h4 className='uppercase'>Transaction History</h4>
+      </div>
+      {/* SEARCH TRANSACTION */}
+        <div className='relative mx-auto mb-3'>
+            <input type="text" className="py-2 px-2 w-[300px] rounded-lg bg-slate-100" placeholder="Search Transactions" />
+            <button className='inset-y-0 absolute right-2 text-2xl text-slate-500'>O</button>
+        </div>
         <div className='flex flex-col'>
             {transactions.map((transaction)=>(
             <Transaction key={transaction.id} id={transaction.id} name={transaction.text} amount={transaction.amount}/>
