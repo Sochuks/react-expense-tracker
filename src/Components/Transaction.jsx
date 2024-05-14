44cac2dc86c6cfac1e7603d1826ce95e11d783cf
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-05-14 10:50:14 +0100

    Modify style to improve UI

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -17,11 +17,12 @@ const Transaction = ({name, amount, id}) => {
   return (
     <div>
         <div className='my-1'>
-            <ul className={`flex justify-between w-full py-3 px-2 bg-slate-100 rounded
-            ${amount < 0 ? 'border-r-4 border-red-500' : 'border-r-4 border-green-700'}`}>
-              {name} <li><span>{sign}${Math.abs(amount)}</span> <button 
-              className=''
-              onClick={()=> deleteTransaction(id)}>Delete</button></li>
+            <ul className={`relative`} id='list'>
+               <li className={`flex justify-between w-full py-3 px-2 bg-slate-100 rounded
+            ${amount < 0 ? 'border-r-4 border-red-500' : 'border-r-4 border-green-700'}`}><span>{name}</span><span>{sign}${Math.abs(amount)}</span></li>
+              <button 
+              className='absolute top-3 -left-4 deleteBtn font-bold text-red-500'
+              onClick={()=> deleteTransaction(id)}>X</button> 
             </ul>
         </div>
     </div>
